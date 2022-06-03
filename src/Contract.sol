b22e24972fc49a83f7751cee3e6388fb6faf7f1d
Gary Thung <garythung@gmail.com>
2022-06-03 11:32:16 -0700

    update licenses, update erc20 for example

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.10;
 
-import "@openzeppelin/token/ERC20/ERC20.sol";
+import "solmate/tokens/ERC20.sol";
 
-contract Contract is ERC20("Token", "TKN") {
+contract Contract is ERC20("Token", "TKN", 18) {
     function mint(address _recipient, uint256 _amount) public {
         _mint(_recipient, _amount);
     }
--- src/test/Utils.sol
+++ src/test/Utils.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.10;
 
 import {Test} from "forge-std/Test.sol";
 
